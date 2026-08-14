cb98e29218fe096570056febede0a5aac0ca58d5
AbdoelMadjid <abdulmadjid.mpd@gmail.com>
2026-08-14 22:12:37 +0700

    Perbaikan tampilan menu cepat di website

--- resources/views/website/templates/unify-education/partials/header.blade.php
+++ resources/views/website/templates/unify-education/partials/header.blade.php
@@ -8,23 +8,23 @@
         <div class="g-bg-main">
             <div class="container g-py-5">
                 <div class="d-flex align-items-center w-100" style="min-height: 40px;">
-                    <!-- Left Side: Intake Button -->
+                    <!-- Left Side: Mobile Jump To & Desktop Intake Button -->
+                    <ul class="list-inline d-flex align-items-center g-mb-0 d-lg-none">
+                        @include('website.templates.unify-education.partials.toolbar.jump-to')
+                    </ul>
+
                     @if(\App\Models\PageConfig\WebFeature::isFeatureActive('intake_button'))
                         <a class="u-link-v5 g-brd-around g-brd-white-opacity-0_2 g-color-white-opacity-0_7 g-color-white--hover g-font-size-12 g-rounded-20 text-uppercase text-nowrap g-px-15 g-py-8 d-none d-lg-inline-block"
                             href="{{ route('website.apply-all-intake') }}">{{ __('website.apply_for_fall_intake') }}</a>
                     @endif
 
-                    <!-- Right Side: Language, Jump-To, Links, Search -->
+                    <!-- Right Side: Language, Links, Search -->
                     <ul class="list-inline d-flex align-items-center g-mb-0 ml-auto">
                         <!-- Language -->
                         @if(\App\Models\PageConfig\WebFeature::isFeatureActive('language_switcher'))
                             @include('website.templates.unify-education.partials.toolbar.language')
                         @endif
                         <!-- End Language -->
-
-                        <!-- Jump To -->
-                        @include('website.templates.unify-education.partials.toolbar.jump-to')
-                        <!-- End Jump To -->
 
                         <!-- Links -->
                         @include('website.templates.unify-education.partials.toolbar.links')
--- resources/views/website/templates/unify-education/partials/toolbar/jump-to.blade.php
+++ resources/views/website/templates/unify-education/partials/toolbar/jump-to.blade.php
@@ -37,19 +37,21 @@
 <li class="list-inline-item g-pos-rel">
     <a id="jump-to-dropdown-invoker"
         class="d-block d-lg-none u-link-v5 g-color-white-opacity-0_7 g-color-white--hover g-font-size-12 text-uppercase g-py-7"
-        href="#" aria-controls="jump-to-dropdown" aria-haspopup="true" aria-expanded="false"
-        data-dropdown-event="hover" data-dropdown-target="#jump-to-dropdown" data-dropdown-type="css-animation"
-        data-dropdown-duration="0" data-dropdown-hide-on-scroll="true" data-dropdown-animation-in="fadeIn"
+        href="javascript:void(0);" aria-controls="jump-to-dropdown" aria-haspopup="true" aria-expanded="false"
+        data-dropdown-event="click" data-dropdown-target="#jump-to-dropdown" data-dropdown-type="css-animation"
+        data-dropdown-duration="200" data-dropdown-hide-on-scroll="false" data-dropdown-animation-in="fadeIn"
         data-dropdown-animation-out="fadeOut">
         {{ __('website.jump_to') }}
         <i class="g-ml-3 fa fa-angle-down"></i>
     </a>
     <ul id="jump-to-dropdown"
-        class="list-unstyled u-shadow-v39 g-brd-around g-brd-4 g-brd-white g-bg-secondary g-pos-abs g-left-0 g-z-index-99 g-mt-13"
+        class="list-unstyled u-shadow-v39 g-brd-around g-brd-4 g-brd-white g-bg-secondary g-pos-abs g-left-0 g-z-index-9999 g-mt-13"
+        style="min-width: 220px; max-width: calc(100vw - 30px);"
         aria-labelledby="jump-to-dropdown-invoker">
         @if(\App\Models\PageConfig\WebFeature::isFeatureActive('intake_button'))
             <li class="dropdown-item g-brd-bottom g-brd-2 g-brd-white g-px-0 g-py-2">
                 <a class="nav-link g-color-main g-color-primary--hover g-bg-secondary-dark-v2--hover g-font-size-default"
+                    style="white-space: normal;"
                     href="{{ route('website.apply-all-intake') }}">{{ __('website.apply_now') }}</a>
             </li>
         @endif
@@ -57,6 +59,7 @@
         @foreach($topNavItems as $nav)
             <li class="dropdown-item g-brd-bottom g-brd-2 g-brd-white g-px-0 g-py-2">
                 <a class="nav-link g-color-main g-color-primary--hover g-bg-secondary-dark-v2--hover g-font-size-default"
+                    style="white-space: normal;"
                     href="{{ $getTopNavUrl($nav) }}" target="{{ $nav->target ?? '_self' }}">
                     {{ $getTopNavTitle($nav) }}
                 </a>
@@ -64,6 +67,7 @@
             @foreach($nav->children->where('is_active', true) as $child)
                 <li class="dropdown-item g-brd-bottom g-brd-2 g-brd-white g-px-0 g-py-2 g-pl-15">
                     <a class="nav-link g-color-main g-color-primary--hover g-bg-secondary-dark-v2--hover g-font-size-default"
+                        style="white-space: normal;"
                         href="{{ $getTopNavUrl($child) }}" target="{{ $child->target ?? '_self' }}">
                         ↳ {{ $getTopNavTitle($child) }}
                     </a>
@@ -75,12 +79,14 @@
             <li class="dropdown-item g-px-0 g-py-2">
                 @auth
                     <a href="{{ route('homepage') }}"
-                        class="nav-link g-color-white g-bg-primary g-bg-primary-light-v1--hover g-font-size-default">
+                        class="nav-link g-color-white g-bg-primary g-bg-primary-light-v1--hover g-font-size-default"
+                        style="white-space: normal;">
                         {{ __('website.dashboard') }}
                     </a>
                 @else
                     <a href="{{ route('login') }}"
-                        class="nav-link g-color-white g-bg-primary g-bg-primary-light-v1--hover g-font-size-default">
+                        class="nav-link g-color-white g-bg-primary g-bg-primary-light-v1--hover g-font-size-default"
+                        style="white-space: normal;">
                         {{ __('website.sign_in') }}
                     </a>
                 @endauth
